6be4565bc94c044e6ec0b7f6253316e2d053c2ef
pan.arno@gmail.com <pan.arno@gmail.com>
2025-07-23 13:26:35 -0700

    Changed Units to Toggle instead of seperate buttons. Added icon through component for unit toggle. Added props to be able to adjust for width and height for icontoggle component. Seperated distance into two imperial and metric. Add numbers. Added step for input for recent length input.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
   <main class="container">
     <section class="d-flex flex-row justify-content-center align-self-stretch mb-3">
       <div v-for="preset in presetsObj" v-bind:key="preset">
-        <button v-on:click="formObj.length = preset.distance" class="btn btn-primary text-capitalize m-2">{{ preset.name }}</button>
+        <button v-on:click="formObj.length = isMetric ? preset.distanceMetric : preset.distanceImperial"
+          class="btn btn-primary text-capitalize m-2">{{ preset.name }}</button>
       </div>
     </section>
     <section class="border">
@@ -15,16 +16,18 @@
         <div class="mb-3 row d-flex flex-row justify-content-end">
           <label
             class="col-sm-2 col-form-label d-flex flex-row justify-content-center align-items-center text-uppercase">Units</label>
-          <button v-on:click="isMetric = true" type="button" class="btn btn-light col-sm-2 border">
-            Metric <span class="badge text-bg-secondary">km</span>
+          <button v-on:click="handleMeasurementSystemToggle" v-if="isMetric" type="button"
+            class="btn btn-light col-sm-2 border">Metric&nbsp;<span class="badge text-bg-secondary">km</span>&nbsp;
+            <IconToggle></IconToggle>
           </button>
-          <button v-on:click="isMetric = false" type="button" class="btn btn-light col-sm-2 border">
-            Imperial <span class="badge text-bg-secondary">mi</span>
+          <button v-on:click="handleMeasurementSystemToggle" v-else type="button"
+            class="btn btn-light col-sm-2 border">Imperial&nbsp;<span class="badge text-bg-secondary">mi</span>&nbsp;
+            <IconToggle></IconToggle>
           </button>
         </div>
         <div class="mb-3 row">
           <label class="col-sm-9 col-form-label">Recent Race Length (i.e. 26.2mi)</label>
-          <input class="col-sm-2" type="number" v-model="formObj.length">
+          <input class="col-sm-2" type="number" step="0.01" v-model="formObj.length" v-on:change="formObj.preset = false">
           <label class="col-sm-1 col-form-label d-flex flex-row justify-content-center align-items-center">{{ isMetric ?
             "km" : "mi" }}</label>
         </div>
@@ -42,9 +45,11 @@
   </main>
 </template>
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
+import IconToggle from './components/icons/IconToggle.vue';
 
 const isMetric = ref(true);
+
 const initialFormObj = {
   length: "",
   metric: isMetric,
@@ -58,26 +63,43 @@ const initialFormObj = {
 const formObj = reactive({
   ...initialFormObj
 })
-const presetsObj = [
+const presetsObj = computed(() => [
   {
     name: "half marathon",
-    distance: isMetric ? 21.1 : 13.1,
+    distanceMetric: 21.10,
+    distanceImperial: 13.10,
   },
   {
     name: 'marathon',
-    distance: isMetric ? 42.2 : 26.2,
+    distanceMetric: 42.20,
+    distanceImperial: 26.20,
   },
   {
     name: '5k',
-    distance: isMetric ? 5 : 3.1,
+    distanceMetric: 5.00,
+    distanceImperial: 3.10,
   },
   {
     name: '10k',
-    distance: isMetric ? 10 : 6.2,
+    distanceMetric: 10.00,
+    distanceImperial: 6.20,
   },
   {
     name: '1 Mile',
-    distance: isMetric ? 1.61 : 5,
+    distanceMetric: 1.61,
+    distanceImperial: 1.00,
   }
-]
+])
+
+const handleMeasurementSystemToggle = () => {
+  if(formObj.length){
+    if(isMetric.value){
+      formObj.length = formObj.length / 1.609
+    } else {
+      formObj.length = formObj.length * 1.609
+    }
+  }
+  isMetric.value = !isMetric.value;
+}
+
 </script>
